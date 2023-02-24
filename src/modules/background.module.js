3be23f43d9e41b626ab3e9b0b69eacf6911cd735
LevitD <diml71@mail.ru>
2023-02-24 22:09:29 +0300

    background-module

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,15 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class BackgroundModule extends Module {
+    trigger() {
+        document.body.style.backgroundColor = `rgb(${this.getRGBColor()})`;
+    }
 
+    getOneColor(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    getRGBColor() {
+        return `${this.getOneColor(255)},${this.getOneColor(255)},${this.getOneColor(255)}`;
+    }
 }
