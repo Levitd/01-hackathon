a2fedef407af802533bd7b1820a8b9a5d6c23d28
LevitD <diml71@mail.ru>
2023-02-24 18:32:51 +0300

    menu module

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
-import './styles.css'
+import './styles.css';
+import './menu.css';
 
-console.log('test app.js');
+import { ContextMenu } from './menu';
+import { BackgroundModule } from './modules/background.module';
+import { ClicksModule } from './modules/clicks.module';
+import { ShapeModule } from './modules/shape.module';
+
+const contextMenu = new ContextMenu('.menu');
+
+const backgroundModule = new BackgroundModule('backgroundModule', 'Случайный фон');
+contextMenu.add(backgroundModule);
+
+const clicksModule = new ClicksModule('clicksModule', 'Аналитика кликов');
+contextMenu.add(clicksModule);
+
+const shapeModule = new ShapeModule('shapeModule', 'Случайная фигура');
+contextMenu.add(shapeModule);
+
+
+// const menuObj = [
+//     { type: clicksModule, text: 'Аналитика кликов ' },
+//     { type: shapeModule, text: 'Случайная фигура' },
+//     { type: 3, text: 'Таймер отсчета' },
+//     { type: 4, text: 'Случайный звук' },
+//     { type: backgroundModule, text: 'Случайный фон' },
+//     { type: 6, text: 'Кастомное сообщение' },
+// ]
+//contextMenu.add(menuObj);
+
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,40 @@
-import {Menu} from './core/menu'
+import { Menu } from './core/menu'
+import { Module } from './core/module';
 
 export class ContextMenu extends Menu {
+    constructor(selector) {
+        super(selector);
 
-}
+        this.selector = selector;
+        this.root = document.querySelector(this.selector);
+
+        document.body.addEventListener('contextmenu', event => {
+            event.preventDefault();
+            this.open(event);
+        })
+    }
+
+    open(event) {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        console.log('ContextMenu', 'open', mouseX, mouseY, this.root);
+        this.root.style.left = `${mouseX}px`;
+        this.root.style.top = `${mouseY}px`;
+        this.root.classList.remove('d-none');
+    }
+
+    close() {
+        console.log('ContextMenu', 'close');
+        this.root.classList.add('d-none');
+    }
+
+    menuHtml(type) {
+        this.root.classList.add('d-none');
+        const itemITML = type.toHTML();
+        this.root.innerHTML = this.root.innerHTML + itemITML;
+    };
+    add(type) {
+        if (type instanceof Module)
+            this.menuHtml(type);
+    }
+};
