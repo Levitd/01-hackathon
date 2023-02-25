af131cca965462353445b4d63779d979a9b2f2e1
Алексей@HP <https://github.com/AkselRus>
2023-02-25 20:43:16 +0300

    job module timer

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -6,23 +6,24 @@ export class TimerModule extends Module {
     }
     createElement(){
         let body = document.querySelector('body');
-        let element = `<div class="timer-block">
+        let element = `<form class="timer-block">
         <div class="timer-1">
-        <span class="timer"></span>
+        <span class="timer-span"></span>
         </div>
         <div class="timer-2">
         <input name="name" class="timer_input" type="text" placeholder="Введите любое число"  value="10"  >
         <button type="submit" class="timer_button">Отправить</button>
         </div>
-        </div> `
-    body.insertAdjacentHTML("beforeend", element);
-    document.addEventListener('submit',(event) => {
+        </form>`
+        
+    body.insertAdjacentHTML("beforeend", element);    
+    body.addEventListener('submit',(event) => {
         event.preventDefault();    
         let input = document.querySelector('.timer_input')
         const valueInput = input.value;
         console.log(event)
         if(valueInput){
-            //this.startTime(valueInput);
+            this.startTime(valueInput);
             console.log(valueInput)
         }
     })
@@ -36,9 +37,12 @@ export class TimerModule extends Module {
         return `${minutes}:${seconds}`;
     }
     startTime(time){
+        let div2 = document.querySelector('.timer-2');
+        div2.remove();
+
         let timerInterval = setInterval(() => {
             time -= 1;
-            let span = document.querySelector('.timer')
+            let span = document.querySelector('.timer-span')
             span.textContent = this.formatTime(time);
         
             if(time === -1){
