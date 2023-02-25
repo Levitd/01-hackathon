df1fd01d0fac6f215b3ba29f633b9d8b7aac0f30
Алексей@HP <https://github.com/AkselRus>
2023-02-25 19:47:13 +0300

    problem event submit

--- src/modules/timer.module.js
+++ src/modules/timer.module.js
@@ -1,26 +1,32 @@
 import { Module } from '../core/module'
 
 export class TimerModule extends Module {
-    trigger(){
-        let countDown = prompt('Введите колличество секунд:');
-
-        let body = document.querySelector('./body');
-        let divTimer = document.createElement('div');
-        divTimer.className = 'timer';
-    
-        let div = document.createElement('div');
-        div.className = 'num';
-
-        let span = document.createElement('span')
-        span.textContent = formatTime(countDown);
-
-        body.prepend(divTimer);
-        divTimer.prepend(div);
-        div.prepend(span)
-
-        startTime(countDown);
+    trigger(){      
+        this.createElement();
     }
-
+    createElement(){
+        let body = document.querySelector('body');
+        let element = `<div class="timer-block">
+        <div class="timer-1">
+        <span class="timer"></span>
+        </div>
+        <div class="timer-2">
+        <input name="name" class="timer_input" type="text" placeholder="Введите любое число"  value="10"  >
+        <button type="submit" class="timer_button">Отправить</button>
+        </div>
+        </div> `
+    body.insertAdjacentHTML("beforeend", element);
+    document.addEventListener('submit',(event) => {
+        event.preventDefault();    
+        let input = document.querySelector('.timer_input')
+        const valueInput = input.value;
+        console.log(event)
+        if(valueInput){
+            //this.startTime(valueInput);
+            console.log(valueInput)
+        }
+    })
+    }
     formatTime(time) {
         const minutes = Math.floor(time / 60);        
         let seconds = time % 60;        
@@ -30,16 +36,19 @@ export class TimerModule extends Module {
         return `${minutes}:${seconds}`;
     }
     startTime(time){
-        timerInterval = setInterval(() => {
+        let timerInterval = setInterval(() => {
             time -= 1;
-            span.textContent = formatTime(time);
+            let span = document.querySelector('.timer')
+            span.textContent = this.formatTime(time);
         
             if(time === -1){
+                let divTimer = document.querySelector('.timer-block');
+                clearInterval(timerInterval);
                 divTimer.remove();
+                
             }
         }, 1000);
+
         return timerInterval;
     } 
-    
-
 }
