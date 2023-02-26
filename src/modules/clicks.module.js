74a4d4e85850d2e8c3d6a59d6cb8905f49d72d2a
LevitD <diml71@mail.ru>
2023-02-26 21:55:08 +0300

    clear code

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -6,7 +6,6 @@ import { disableItemMenu } from '../utils';
 
 const customMessage = new CustomMessage('customMessage', 'Вызвать сообщение');
 const timerModule = new TimerModule('timerModule', 'Таймер отсчета',);
-//ToDo импорт сообщений, и потом в эти сообщения выводить 
 
 export class ClicksModule extends Module {
     constructor(type, text, timerMSec, contextMenu) {
@@ -14,22 +13,19 @@ export class ClicksModule extends Module {
         this.type = type;
         this.timerinMiliSec = timerMSec;
         this.contextMenu = contextMenu;
-        console.log(this.contextMenu)
     }
     trigger(event) {
         localStorage.setItem(`countClickArray`, JSON.stringify([1, 0, 0]));
         timerModule.createElement();
         disableItemMenu(this.type);
         timerModule.startTime(this.timerinMiliSec / 1000);
-        const setTtimeoutID = window.setTimeout(this.stopClick, this.timerinMiliSec, this.timerinMiliSec, this.messageClick, this.type);
+        setTimeout(this.stopClick, this.timerinMiliSec, this.timerinMiliSec, this.messageClick, this.type);
     }
     stopClick(times, callback, type) {
         const countClickArray = JSON.parse(localStorage.getItem(`countClickArray`));
-        console.log(countClickArray, times);
         callback(`За ${numFormat(times / 1000, 0)} секунд вы кликнули: ${countClickArray[1]} раз левой и ${countClickArray[2]} раз правой кнопкой мыши.`, type);
     }
     messageClick(text, type) {
-        console.log(text);
         customMessage.createBlockMessage(text);
         customMessage.deleteBlock(5000);
         disableItemMenu(type);
